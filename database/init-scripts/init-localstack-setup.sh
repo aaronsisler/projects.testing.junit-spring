ffb62d3dd120fee5c750f614b04f3919acb1e370
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2024-08-18 15:18:19 -0400

    Everything in working order for Localstack and DDB

--- database/init-scripts/init-localstack-setup.sh
+++ database/init-scripts/init-localstack-setup.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-echo "I am here"
+echo "Begin: DynamoDB -> Create Table"
+awslocal dynamodb create-table \
+    --cli-input-json file://dynamodb-table-definition.json \
+    --region us-east-1
+echo "End: DynamoDB -> Create Table"
 #
-#awslocal dynamodb list-tables
+echo "Begin: DynamoDB -> List Tables"
+#
+awslocal dynamodb list-tables
+#
+echo "End: DynamoDB -> List Tables"
+#
+# The below isn't used currently but it was working before
+# Starting and using Cloudformation took longer than
+# just doing create-table using JSON template
+#echo "Begin: Cloudformation -> Deploy"
+#awslocal cloudformation deploy \
+#    --stack-name aws-dynamodb-cfn-table \
+#    --template-file "dynamo-db.yaml"
+#echo "End: Cloudformation -> Deploy"

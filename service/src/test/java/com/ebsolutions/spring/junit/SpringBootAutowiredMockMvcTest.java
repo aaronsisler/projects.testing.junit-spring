82b50a95fd547b3724f7396fc53c38b67da0f413
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2024-08-22 19:01:43 -0400

    Adding in the Info endpoint check

--- service/src/test/java/com/ebsolutions/spring/junit/SpringBootAutowiredMockMvcTest.java
+++ service/src/test/java/com/ebsolutions/spring/junit/SpringBootAutowiredMockMvcTest.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +22,24 @@ public class SpringBootAutowiredMockMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private final String HEALTH_CHECK_URL = "/actuator/health";
+    private final String INFO_CHECK_URL = "/actuator/info";
+
     @Test
-    void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/actuator/health"))
+    void whenHealthCheckIsCalledShouldReturnCorrectMessage() throws Exception {
+        this.mockMvc.perform(get(HEALTH_CHECK_URL))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("UP")));
+    }
+
+    @Test
+    void whenInfoCheckIsCalledShouldReturnCorrectMessage() throws Exception {
+        this.mockMvc.perform(get(INFO_CHECK_URL))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.build.group", is("com.ebsolutions.spring.junit")))
+                .andExpect(jsonPath("$.build.artifact", is("spring-junit-service")))
+                .andExpect(jsonPath("$.build.name", is("Spring Junit Service")))
+                .andExpect(jsonPath("$.build.version", notNullValue()))
+                .andExpect(jsonPath("$.build.time", notNullValue()));
     }
 }

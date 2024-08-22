98f589396d03460ccf60a7c8c38b91b12b4a5ff5
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2024-08-22 18:35:12 -0400

    Base test is working. MVC next

---
+++ service/src/test/java/com/ebsolutions/spring/junit/EmptySpringBootTest.java
@@ -0,0 +1,12 @@
+package com.ebsolutions.spring.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmptySpringBootTest {
+
+    @Test
+    void contextLoads() {
+    }
+}

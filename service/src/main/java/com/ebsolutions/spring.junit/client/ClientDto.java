c4e2840cf0d2357e4078818a68fe3827cba306ee
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2024-08-25 17:08:07 -0400

    Updating to get rid of build and OpenApi warnings

--- service/src/main/java/com/ebsolutions/spring.junit/client/ClientDto.java
+++ service/src/main/java/com/ebsolutions/spring.junit/client/ClientDto.java
@@ -3,6 +3,7 @@ package com.ebsolutions.spring.junit.client;
 import com.ebsolutions.spring.junit.shared.DatabaseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
@@ -12,5 +13,6 @@ import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 @DynamoDbBean
 @SuperBuilder
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class ClientDto extends DatabaseDto {
 }
